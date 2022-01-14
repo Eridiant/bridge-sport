24cda63cab747874257c1003574e9ac61b189a87
Eridiant <eridiante@gmail.com>
2022-01-14 13:53:24 +0300

    f fix style

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -12,15 +12,39 @@ $this->title = 'My Yii Application';
         <a href="<?= Url::to(['/site/contact']) ?>"><?= Url::to(['/site/contact']) ?></a>
     </p>
     <?php foreach ($model as $m): ?>
-        <div>
-            <h1><?= $m->name; ?></h1>
-            <p><?= $m->preview; ?></p>
-            <p><?php 
-            var_dump('<pre>');
-            var_dump($m->attributes); 
-            var_dump('</pre>');
-            ?></p>
-            <a href="<?= Url::to(['/news', 'id' => $m->id]) ?>">читать больше</a>
+        <div class="news-wrapper">
+            <?php if (empty($m->img)):
+                switch ($m->category_id) {
+                    case 0:
+                        $img = 'play';
+                        break;
+                    case 1:
+                        $img = 'whist';
+                        break;
+                    case 2:
+                        $img = 'competition';
+                        break;
+                    default:
+                        $img = 'competition';
+                        break;
+                }
+                ?>
+                <img src="/images/dummy/<?= $img; ?>.jpg" alt="" class="news-img">
+            <?php else: ?>
+                <img src="/images/dummy/<?= $m->img; ?>" alt="">
+            <?php endif; ?>
+            <div class="news-inner">
+                <h2><?= $m->name; ?></h2>
+                <p><?= $m->preview; ?></p>
+                <p><?php 
+                // var_dump('<pre>');
+                // var_dump($m->category_id); 
+                // var_dump('</pre>');
+                ?></p>
+                <a href="<?= Url::to(['/news', 'id' => $m->id]) ?>">
+                    читать больше
+                </a>
+            </div>
         </div>
     <?php endforeach; ?>
 </div>
