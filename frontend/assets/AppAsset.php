4dfe8184938adf1994a22f16f0b52b0ddcfe99df
Eridiant <eridiante@gmail.com>
2022-01-16 12:33:56 +0300

    f disble bootstrap

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -20,4 +20,13 @@ class AppAsset extends AssetBundle
         'yii\web\YiiAsset',
         'yii\bootstrap4\BootstrapAsset',
     ];
+    public function init()
+    {
+        parent::init();
+        // resetting BootstrapAsset to not load own css files
+        \Yii::$app->assetManager->bundles['yii\\bootstrap4\\BootstrapAsset'] = [
+            'css' => [],
+            'js' => []
+        ];
+    }
 }
