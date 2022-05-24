6edae44d5c2848d7f1721778b6942627bd1b7306
Eridiant <eridiante@gmail.com>
2022-05-24 14:47:14 +0300

    add migration

---
+++ console/migrations/m220524_114009_create_proc_user_ip_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%proc_user_ip}}`.
+ */
+class m220524_114009_create_proc_user_ip_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%proc_user_ip}}', [
+            'id' => $this->primaryKey(),
+            'ip' => $this->binary()->notNull(),
+            'ref' => $this->string(255),
+            'code' => $this->string(10),
+            'region' => $this->string(128),
+            'country' => $this->string(64),
+            'city' => $this->string(64),
+            'created_at' => $this->integer()->notNull(),
+        ]);
+
+        // creates index for column `ip`
+        $this->createIndex(
+            '{{%idx-proc-user-ip-ip}}',
+            '{{%proc_user_ip}}',
+            'ip'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops index for column `ip`
+        $this->dropIndex(
+            '{{%idx-proc-user-ip-ip}}',
+            '{{%proc_user_ip}}',
+            'ip'
+        );
+
+        $this->dropTable('{{%proc_user_ip}}');
+    }
+}
