4047c03d52c9fa7ea1698f57f671169ee187b6a0
Eridiant <eridiante@gmail.com>
2022-06-29 17:50:27 +0300

    cp fix img

--- common/components/image/index.php
+++ common/components/image/index.php
@@ -3,6 +3,7 @@
 preg_match('/[a-z]{2,}$/', $model->format, $img);
 
 ?>
-<?= $source; ?>
+
 <?= $sources; ?>
-<img src="/images/<?= "{$model->path}-{$model->width}x{$model->height}.{$img[0]}"; ?>" alt="<?= $model->alt; ?>" width="<?= $model->width; ?>" height="<?= $model->height; ?>">
+<?= $source; ?>
+<img src='/images/<?= "{$model->path}-{$model->width}x{$model->height}.{$img[0]}"; ?>' alt="<?= $model->alt; ?>" width="<?= $model->width; ?>" height="<?= $model->height; ?>">
