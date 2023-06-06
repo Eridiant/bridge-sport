24f351c9b75e95656aced9edb1ba72015178dbd9
Eridiant <eridiante@gmail.com>
2023-06-06 14:04:01 +0300

    sitemap add index

--- frontend/views/seo/index.php
+++ frontend/views/seo/index.php
@@ -2,6 +2,12 @@
 
 ?>
 <?= '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ; ?>
+    <url>
+        <loc><?= $host; ?></loc>  
+        <lastmod><?= date(DATE_W3C, $posts[0]->updated_at ?? 0);date('Y-m-d') ?></lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+    </url>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach ($posts as $post): ?>
         <url>
